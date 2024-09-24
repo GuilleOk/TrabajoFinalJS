0dc679f0670b2e18b348c46dc18e64e6867ecc4a
GuilleOk <fernandez.martinez1403@gmail.com>
2024-09-23 21:13:54 -0400

    Formulario a la mitad

--- js/main.js
+++ js/main.js
@@ -41,7 +41,20 @@ let nombre = document.getElementById('nombre');
 let apellidos = document.getElementById('apellidos');
 let telefono = document.getElementById('telef');
 let email = document.getElementById('email');
+let producto = document.getElementById('producto');
+let plazos = document.getElementById('plazos');
 let formulario = document.querySelector('form');
+
+let valida = {
+    nombre: false,
+    apellidos: false,
+    telefono: false,
+    email: false,
+    producto: false,
+    plazos: false,
+    presupuesto: false,
+    condiciones: false
+};
 
 let setError = (etiqueta, mensaje)=>{
     let formControl = etiqueta.parentElement;
@@ -64,81 +77,43 @@ let setSuccess = (etiqueta)=>{
     formControl.className = 'inputOk';
 }
 
-let valida = {
-    nombre: false,
-    apellidos: false,
-    telefono: false,
-    email: false
-};
-
-nombre.addEventListener('blur',()=>{
-    let nombreOk = /^[a-zA-Z áéíóúÁÉÍÓÚ]{2,30}$/;
-
-    if (nombre.value == '' || nombre.value == null) {
-        valida.nombre = false;
-        setError(nombre,'Este campo no puede estar vacío');
+function validarCampo (input,mensaje1, mensaje2, reglavalid){
+    input.addEventListener('blur',()=>{
+        let inputOk = reglavalid;
+    if (input.value == '' || input.value == null) {
+        valida.input = false;
+        setError(input,mensaje1);
     }else{
-        if (!nombreOk.exec(nombre.value)) {
-            valida.nombre = false;
-            setError(nombre,'Este campo solo acepta letras mayúsculas, minúsculas y espacios')
+        if (!inputOk.exec(input.value)) {
+            valida.input = false;
+            setError(input,mensaje2)
         }else{
-            valida.nombre = true;
-            setSuccess(nombre);
+            valida.input = true;
+            setSuccess(input);
         }
     }
+    })
 }
-);
 
-apellidos.addEventListener('blur',()=>{
-    let apellidosOk = /^[a-zA-Z áéíóúÁÉÍÓÚ]{2,30}$/;
+let nombreOk = /^[A-Za-z áéíóúÁÉÍÓÚ]{2,30}$/;
 
-    if (apellidos.value == '' || apellidos.value == null) {
-        valida.apellidos = false;
-        setError(apellidos,'Este campo no puede estar vacío');
-    }else{
-        if (!apellidosOk.exec(apellidos.value)) {
-            valida.apellidos = false;
-            setError(apellidos,'Este campo solo acepta letras mayúsculas, minúsculas y espacios')
-        }else{
-            valida.apellidos = true;
-            setSuccess(apellidos);
-        }
+validarCampo(nombre,'Este campo no puede estar vacío','Este campo solo acepta letras mayúsculas, minúsculas y espacios',nombreOk)
+
+validarCampo(apellidos,'Este campo no puede estar vacío','Este campo solo acepta letras mayúsculas, minúsculas y espacios',nombreOk)
+
+let telefonoOk = /^[0-9]{8}$/;
+
+validarCampo(telefono, 'Este campo no puede estar vacío', 'Este campo solo acepta 8 números',telefonoOk);
+
+let emailOk = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+validarCampo(email, 'Este campo no puede estar vacío', 'Por favor introduzca una dirección de correo electrónico válida', emailOk)
+
+producto.addEventListener('change',()=>{
+    for (let i = 0; i < producto.length; i++) {
+        if (producto[i].selected) {
+            console.log(producto[i].value)
+        };
+        
     }
-}
-)
-
-telefono.addEventListener('blur',()=>{
-    let telefonoOk = /^[0-9]{8}$/;
-
-    if (telefono.value == '' || telefono.value == null) {
-        valida.telefono = false;
-        setError(telefono,'Este campo no puede estar vacío');
-    }else{
-        if (!telefonoOk.exec(telefono.value)) {
-            valida.telefono = false;
-            setError(telefono,'Este campo solo acepta 8 números')
-        }else{
-            valida.telefono = true;
-            setSuccess(telefono);
-        }
-    }
-}
-)
-
-email.addEventListener('blur',()=>{
-    let emailOk = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (email.value == '' || email.value == null) {
-        valida.email = false;
-        setError(email,'Este campo no puede estar vacío');
-    }else{
-        if (!emailOk.exec(email.value)) {
-            valida.email = false;
-            setError(email,'Este campo solo acepta 8 números')
-        }else{
-            valida.email = true;
-            setSuccess(email);
-        }
-    }
-}
-)
+})
